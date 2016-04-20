461ab3032378afa7e922487d3572f166731772d6
Stanislav Filin <s.filin@bvblogic.com>
2016-04-21 00:10:13 +0300

    ticket:1

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 MANAGE=django-admin.py
 SETTINGS=fortytwo_test_task.settings
 
+test:
+	PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=$(SETTINGS) $(MANAGE) test
+
 run:
 	PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=$(SETTINGS) $(MANAGE) runserver
 
--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,6 +1,8 @@
 from django.test.testcases import TestCase
 from django.core.urlresolvers import reverse
 
+from apps.hello.models import Persons
+
 
 class IndexTest(TestCase):
 
@@ -8,3 +10,22 @@ class IndexTest(TestCase):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
 
+    def test_create_model(self):
+        Persons.objects.create(first_name='Stanislav',
+                               last_name='Filin',
+                               date='1994-04-20',
+                               bio='Chernivtsi City',
+                               email='stasfilinmusic@gmail.com',
+                               jabber='stasfilin@qutim.org',
+                               skype='stas_filin',
+                               other='Telegram: @stasfilin'
+                               )
+        self.assertEqual(bool(Persons.objects.filter(first_name='Stanislav', last_name='Filin')), True)
+
+    def test_admin_login(self):
+        data = {
+            'name': 'admin',
+            'password': 'admin'
+        }
+        response = self.client.post(reverse('admin:index'), data)
+        self.assertEqual(response.status_code, 200)
